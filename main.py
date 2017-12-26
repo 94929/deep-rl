f5d341a7ead907f2425c2ac5827c50b2a77bc559
Jinsung Ha <jsung5381@naver.com>
2017-12-26 16:39:14 +0000

    Dummy Q-learning (table)

--- main.py
+++ main.py
@@ -1,27 +1,78 @@
 import gym
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-env = gym.make('FrozenLake-v0') # Create the environment, env of FrozenLake Game
-env.reset()                     # Reset env to initial observation(state)
+from gym.envs.registration import register
 
-action_dict = {0:'LEFT', 1:'DOWN', 2:'RIGHT', 3:'UP'}
+# A helper function which randomly picks an aciton between argmaxed indices
+# Refered from https://gist.github.com/stober/1943451
+def random_argmax(vector):
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0]
+    return random.choice(indices)
 
-while True:
-    # Agent takes the following action
-    action = env.action_space.sample()
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'is_slippery':False}
+)
 
-    # After each action, env renders(display) the current observation
-    env.render()
+env = gym.make('FrozenLake-v3')
 
-    # state(i.e. observation) (object): representing your observation of the environment
-    # reward (float): amount of reward achieved by the previous action
-    # done (bool): indicates whether it's time to reset the environment again
-    # info (dict): diagnostic information useful for debugging
-    state, reward, done, info = env.step(action)
+""" The environment looks like as below
 
-    # Performing an Action will result State and Reward
-    print('Action: {}, State: {}, Reward: {}'.format(action_dict[action], state, reward))
+    s0 , s1 , s2 , s3 ,
+    s4 , s5 , s6 , s7 ,
+    s8 , s9 , s10, s11,
+    s12, s13, s14, s15,
 
-    if done:
-        print('Finished with reward', reward)
-        break
+    And each state, you can perform one of LEFT(0), DOWN(1), RIGHT(2), UP(3)
+
+"""
+
+nb_states = env.observation_space.n     # 16
+nb_actions = env.action_space.n         # 4
+
+# Initialise Q-Table (16x4 sized) with zeros
+Q = np.zeros([nb_states, nb_actions])
+
+# Set number of iterations
+nb_episodes = 1500
+
+# Create a list to contain cumulative rewards per episode
+cumulative_rewards = []
+
+for _ in range(nb_episodes):
+    
+    state = env.reset()     # Init env and receive initial state of the agent
+    cumulative_reward = 0   # Set the cumulative reward to be zero
+    done = False            # Set done to False, is required to enter the loop
+
+    # Q-Table learning algorithm
+    while not done:
+        # Choose an action from the current state
+        action = random_argmax(Q[state, :])
+
+        # Receive a feedback after taking the action
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table using the Q-Learning formula 
+        Q[state, action] = reward + np.max(Q[new_state, :])
+
+        cumulative_reward += reward
+        state = new_state
+
+    cumulative_rewards.append(cumulative_reward)
+
+# The success rate can be calculated as below because 'if succ then reward=1 else reward=0'
+succ_rate = sum(cumulative_rewards) / nb_episodes
+print('Success rate:', succ_rate)
+print('Final Q-Table values')
+print('Left Down Right Up')
+print(Q)
+
+# Print result
+plt.bar(range(len(cumulative_rewards)), cumulative_rewards, color='blue')
+plt.show()
 
