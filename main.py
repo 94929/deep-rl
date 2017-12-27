2936ac5c97430d9fe2a91ccecd7541eb5bd0ba3d
Jinsung Ha <jsung5381@naver.com>
2017-12-27 02:17:59 +0000

    Q-learning (table) Stocastic (non-deterministic) world

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import gym
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-from gym.envs.registration import register
+# Define our world to be non-deterministic(i.e. stochastic), 'is_slippery=True'
+env = gym.make('FrozenLake-v0')
 
-# Environment set up
-register(
-    id='FrozenLake-v3',
-    entry_point='gym.envs.toy_text:FrozenLakeEnv',
-    kwargs={'is_slippery':False}
-)
-
-env = gym.make('FrozenLake-v3')
-
-# Set parameters for Q-Table
-nb_states = env.observation_space.n     # 16
-nb_actions = env.action_space.n         # 4
-
-# Initialise Q-Table (16x4 sized) with zeros
+# Initialise Q-Table with zeros representing rewards
+nb_states = env.observation_space.n
+nb_actions = env.action_space.n
 Q = np.zeros([nb_states, nb_actions])
 
+# Set learning rate, alpha representing how much the agent listen to Q-Table
+alpha = .85
+
 # Set discount reward factor, gamma
-gamma = 0.9
+gamma = .99
 
 # Set number of iterations
 nb_episodes = 1500
@@ -36,37 +28,24 @@ for i in range(nb_episodes):
     cumulative_reward = 0   # Set the cumulative reward to be zero
     done = False            # Set done to False, is required to enter the loop
 
-    e = 1. / ((i//100)+1)   # Set decaying e value for our algorithm
-
-    """ Why do we do 'i // 100'?
-
-    # Setting reasonable e is crucial because it will affect agent's preference.
-    # If e is too small, it tends to exploit (a lot) rather than explorate env.
-    # Meaning that it won't know what to do when rewards from actions are the same.
-    # Same problem as before, of 'action = np.argmax(Q[state, :])'
-    
-    """
-
     # The Q-Table learning algorithm with discounted reward
-    # Q(s, a) = reward + gamma * max.Q'(s', a')
     while not done:
         # Choose an action which maximises the reward (from the current state)
-        # When choosing an action, use decaying e-greedy algorithm
-        if np.random.rand(1) < e:
-            action = env.action_space.sample()
-        else:
-            action = np.argmax(Q[state, :])
+        # When choosing an action, use decaying random noise for path exploration
+        action = np.argmax(Q[state, :] + np.random.randn(1, nb_actions) / (i + 1))
 
         # Receive a feedback after taking the action
         new_state, reward, done, _ = env.step(action)
 
-        # Update Q-Table using the algorithm with discounted reward factor
-        # Q(s, a) = reward + gamma * max(a').Q'(s', a')
-        Q[state, action] = reward + gamma * np.max(Q[new_state, :])
+        # In stochastic world, we need not to rely on previous Q-Table
+        # This can be done by adding the learning rate variable, alpha
+        # Q(s, a) = (1-alpha)*Q(s, a) + alpha*(reward + gamma*max(a').Q(s', a')
+        Q[state, action] = (1 - alpha) * Q[state, action] \
+                            + alpha * (reward + gamma * np.max(Q[new_state, :]))
 
         cumulative_reward += reward
         state = new_state
-
+    
     cumulative_rewards.append(cumulative_reward)
 
 # Print the result
