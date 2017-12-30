f0d6f2bd4c4ec61cb9e39b8e8d04b45cd105f220
Jinsung Ha <jsung5381@naver.com>
2017-12-30 23:42:04 +0000

    Q-learning (network) - CartPole

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import gym
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-# Define our world to be non-deterministic(i.e. stochastic), 'is_slippery=True'
-env = gym.make('FrozenLake-v0')
+# Define our world to be 'CartPole'
+env = gym.make('CartPole-v0')
 
 # Set input and output size used for Q-Network
-input_size  = env.observation_space.n
-output_size = env.action_space.n
-
-def one_hot(x):
-    return np.identity(input_size)[x : x+1]
+input_size  = env.observation_space.shape[0] # (4,)
+output_size = env.action_space.n             # 2
 
 # Set learning rate, alpha
-alpha = .1
+alpha = 1e-1
 
 # Set discount (reward) factor, gamma
 gamma = .99
@@ -24,13 +20,10 @@ nb_episodes = 1500
 
 """ The Q-Network 
 
-    The network inputs any state from 0 to 15.
-    The network outputs an action from 0 to 3.
+    X = Input layer
+    W = The first Hidden layer
 
-    X = Input layer, 1x16 sized matrix
-    W = The first Hidden layer, 16x4 sized matrix
-
-    Qpred = Predicted value, 1x4 sized matrix
+    Qpred = Predicted value
     Y = Expected value
 
     loss = diff = sum $ square $ Q - Y
@@ -40,81 +33,88 @@ nb_episodes = 1500
 # #############################################################################
 
 # Define layer parameters
-X = S = tf.placeholder(shape=[1, input_size], dtype=tf.float32)
-W = tf.Variable(tf.random_uniform(shape=[input_size, output_size], minval=0, maxval=0.01))
+X = tf.placeholder(shape=[None, input_size], dtype=tf.float32)
 
-Qpred = tf.matmul(X, W)
-Y = tf.placeholder(shape=[1, output_size], dtype=tf.float32)
+# First layer
+W1 = tf.get_variable(name='W1', shape=[input_size, output_size], \
+                    initializer=tf.contrib.layers.xavier_initializer())
+
+Qpred = tf.matmul(X, W1)
+Y = tf.placeholder(shape=[None, output_size], dtype=tf.float32)
 
 # Now, calculaing the loss, it's matrix subtraction, use square and reduce_sum of tf
 loss = diff = tf.reduce_sum(tf.square(Qpred - Y))
 
 # Train using GradientDescentOptimizer with given learning_rate and loss obtained above
-train = tf.train.GradientDescentOptimizer(learning_rate=alpha).minimize(loss)
+train = tf.train.AdamOptimizer(learning_rate=alpha).minimize(loss)
 
 # #############################################################################
 
-# Create a list to contain cumulative rewards per episode
-cumulative_rewards = []
-
-# Create new Session, sess
+results = []
 with tf.Session() as sess:
     
-    init = tf.global_variables_initializer()
-    sess.run(init)
+    sess.run(tf.global_variables_initializer())
     for i in range(nb_episodes):
     
-        state = env.reset()     # Reset env and get the initial observation
-        epsilon = .1 / (i+1)    # Use epsilon greedy algorithm for exploration
-        cumulative_reward = 0   # Set the cumulative reward to be zero
-        done = False            # Set done to False, is required to enter the loop
+        state = env.reset()         
+        epsilon = .1 / ((i//50)+1)  
+        done = False                
+        
+        # Set step_count used for checking our network
+        step_count = 0 
 
-        # The Q-Network learning (training) algorithm is splited into two parts
-        # (1) Choose an action at each step, (2) Save the reward derived
+        # The Q-Network learning (training) algorithm 
         while not done:
+            step_count += 1
 
-            # (1) Choose an action
-            # The line below predicts Q values from a given state
-            # The Q contains expected rewards of each action from given state
-            # e.g. Q = [[ 0.00420892  0.00085265  0.00160731  0.49378362]]
-            Q = sess.run(Qpred, feed_dict={X:one_hot(state)})
-            #print('Qs value: ', Q)
+            # (1) Choose an action with epsilon greedy algorithm
             if np.random.rand(1) < epsilon:
                 # Take an action randomly for exloration purpose
                 action = env.action_space.sample()
             else:
-                # Take an action which yields the best reward according to Q
+                # Preprocess(i.e. reshpae) the input, state
+                x = np.reshape(state, [1, input_size])
+
+                # By using our network defined Qpred,
+                # Obtain Q values (i.e. rewards) when the agent is at state s
+                Q = sess.run(Qpred, feed_dict={X:x})
+
+                # Choose action which maximises the reward
                 action = np.argmax(Q)
 
-            # After taking an action, get new state and reward
-            new_state, reward, done, _ = env.step(action)
-
-            # (2) Save the reward, it's done according to the equation below
-            # reward if terminal_state else reward + gamma*max(Q)
+            # (2) Update the reward into Q-Network
+            next_state, reward, done, _ = env.step(action)
             if done:
-                # Q[0, _] is crucial since Qpred is 1x4 matrix of [[a1, a2 ,a3, a4]]
-                Q[0, action] = reward
+                # In env, we don't want the game to be done, if done give penalty
+                Q[0, action] = -100
             else:
-                # Obtain new Q values by feeding the new state through our network
-                # Same as for the above Q obtained, but this is Q for new state
-                Qtmp = sess.run(Qpred, feed_dict={X:one_hot(new_state)})
-                #print('Qtmp: ', Qtmp)
-                # Update Q
-                Q[0, action] = reward + gamma*np.max(Qtmp)
+                next_x = np.reshape(next_state, [1, input_size])
+                next_Q = sess.run(Qpred, feed_dict={X:next_x})
 
-            # 'Train' our network using target (Y) and predicted (Qpred) values
-            sess.run(train, feed_dict={X:one_hot(state), Y:Q})
+                Q[0, action] = reward + gamma*np.max(next_Q)
 
-            cumulative_reward += reward
-            state = new_state
+            # Train our network using target (Y) and predicted (Qpred) values
+            # i.e. Telling the network that when input:x we expect output:Q
+            # the network will adjust its values in the network according to the above
+            sess.run(train, feed_dict={X:x, Y:Q})
+
+            state = next_state
     
-        cumulative_rewards.append(cumulative_reward)
+        results.append(step_count)
+        print('Episode: {}, steps: {}'.format(i, step_count))
+        
+        # Finish training if the result seems reasonably good enough
+        # i.e. the last five episodes finished with at least 500 steps
+        if len(results) > 5 and np.mean(results[-5:]) > 500:
+            break
 
-# Print the result
-succ_rate = sum(cumulative_rewards) / nb_episodes
-print('Success rate:', succ_rate)
+    # After training, test our network in action
+    observation = env.reset()
+    done = False
+    while not done:
+        x = np.reshape(observation, [1, input_size])
+        Qs = sess.run(Qpred, feed_dict={X:x})
+        action = np.argmax(Qs)
 
-# Plot the result
-plt.bar(range(len(cumulative_rewards)), cumulative_rewards, color='blue')
-plt.show()
+        observation, _, done, _ = env.step(action)
 
